e21494f157230df5934ada10284db21ac95d02b8
Nursultanakmatbekov <nursultanakmatbekov34@gmail.com>
2024-10-02 15:32:15 +0600

    переделал верстку во всех фрагментах.Скрыл  системный панели (навигационной)

--- app/src/main/java/com/nur/uss/ui/activity/MainActivity.kt
+++ app/src/main/java/com/nur/uss/ui/activity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.nur.uss.ui.activity
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.nur.uss.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,5 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        hideNavigationBar()
+    }
+
+    private fun hideNavigationBar() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 }
